93953447a40577e64952f2ab9a7e9b645e1cf1fe
eliottdouieb <eliottdouieb@gmail.com>
2025-10-21 13:12:10 +0300

    Update interface_streamlit.py

--- interface_streamlit.py
+++ interface_streamlit.py
@@ -4,13 +4,13 @@ st.set_page_config(page_title="Contrôle des écritures comptables", layout="wid
 
 st.title("📊 Interface de Contrôle Comptable")
 
-option = st.radio("Quel type d'écriture souhaites-tu contrôler ?", ["Achats", "Achatsv2","Ventes","Encaissements"], horizontal=True)
+option = st.radio("Quel type d'écriture souhaites-tu contrôler ?", ["Achats","Ventes","Encaissements"], horizontal=True)
 
-if option == "Achats":
-    from controle_achats import run_interface as run_achats
-    run_achats()
+# if option == "Achats":
+#     from controle_achats import run_interface as run_achats
+#     run_achats()
     
-elif option == "Achatsv2":
+if option == "Achats":
     from achats_v2 import run_interface as run_achats
     run_achats()
 
