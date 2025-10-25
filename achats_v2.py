94106fadf3061159d72c7ea80d4ec88b07bb460c
eliottdouieb <eliottdouieb@gmail.com>
2025-10-26 00:28:28 +0300

    Update achats_v2.py

--- achats_v2.py
+++ achats_v2.py
@@ -293,7 +293,7 @@ def check_lignes_comptables(df):
             else:
                 log_piece.append(f"Ce numero de piece a besoin d'une conversion de la devise manuelle ,  Devise : {devise}, Date : {date_facture}, Montant : {original_amount} ")
                 log_ko=True
-
+            continue
         df_provisoire=df[(df["n° de piece"]==i) & (df["Code"]=="G")]
         if check_compte_tiers_invalide(df_provisoire):
             log_piece.append("Compte Tiers invalide")
