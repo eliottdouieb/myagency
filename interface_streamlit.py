90780ae0fb93375c003cb77ceaeeda4062234875
eliottdouieb <eliottdouieb@gmail.com>
2025-10-27 21:59:28 +0200

    Update interface_streamlit.py

--- interface_streamlit.py
+++ interface_streamlit.py
@@ -15,7 +15,7 @@ if option == "Achats":
     run_achats()
 
 elif option == "Ventes":
-    from controle_ventes import run_interface as run_ventes
+    from ventes_v2 import run_interface as run_ventes
     run_ventes()
 
 else :
