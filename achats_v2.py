5945ec1dc937736502fa2b95ff18d221520a259b
eliottdouieb <eliottdouieb@gmail.com>
2025-10-31 00:57:01 +0200

    Update achats_v2.py

--- achats_v2.py
+++ achats_v2.py
@@ -399,6 +399,7 @@ def run_interface():
         log_debut=[]
         log_debut.append(remplie_numero_piece_manquant(df))
         log_debut.append(suppression_445660_dans_Compte_tiers(df))
+        log_debut.append(suppression_caracteres_speciaux(df))
 
         log_generale,Compte_Tiers_invalide,achats_ko=check_lignes_comptables(df)
         st.session_state.df_source = df
