dd0d6e0abd0361606f890f92ccdf06b0fdb11cfd
eliottdouieb <eliottdouieb@gmail.com>
2025-11-11 11:37:50 +0200

    Update encaissements_v2.py

--- encaissements_v2.py
+++ encaissements_v2.py
@@ -352,7 +352,7 @@ def run_interface():
 
         st.write("🔧 Nettoyage de la colonne Name effectué : suppression après '-' et caractères spéciaux retirés.")
 
-        df['Account Client']=df['Account Client'].replace(0,np.nan)
+        df['Account Client']=df['Account Client'].replace('0',np.nan)
         st.write("🔧 Nettoyage de la colonne Account Client effectué : suppression des 0 inutiles.")
         st.write(df['Account Client'].unique())
 
