710a474b4455a8eced53d628ab28861701261a5e
eliottdouieb <eliottdouieb@gmail.com>
2025-11-11 11:30:46 +0200

    Update encaissements_v2.py

--- encaissements_v2.py
+++ encaissements_v2.py
@@ -355,6 +355,8 @@ def run_interface():
         df['Account Client']=df['Account Client'].replace(0,np.nan)
         st.write("🔧 Nettoyage de la colonne Account Client effectué : suppression des 0 inutiles.")
 
+        st.dataframe(df.head(45))
+
         log_generale,Compte_Tiers_invalide,encaissements_ko=check_lignes_comptables(df)
         st.session_state.df_source = df
 
