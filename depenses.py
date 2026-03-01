849d7450dc0dd524d6711c0a2d8ff8401e60b86e
eliottdouieb <eliottdouieb@gmail.com>
2026-03-01 23:44:19 +0200

    new

--- depenses.py
+++ depenses.py
@@ -1016,7 +1016,7 @@ def run_interface():
 
 
 
-    tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs([
+    tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs([
     "✅ Matches & Validation",
     "⚠️ KO Revolut (À traiter)",
     "⚠️ KO BackOffice",
